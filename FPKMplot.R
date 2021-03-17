d5e797c120c38e768f3e7a75aab2a752882827a6
Monil Gandhi <gandhimonil.2008@gmail.com>
2021-03-16 19:30:09 -0700

    Added comments, heatmap and GO csv's

--- FPKMplot.R
+++ FPKMplot.R
@@ -1,44 +1,40 @@
-install.packages("pacman")
+# Monil Gandhi - Biologist
 
-# Then load the package by using either of the following:
-require(pacman)  # Gives a confirmation message.
-library(pacman)  # No message.
+#File Description - Created line plots for the different phases of Sacromere, Mitochondria and Cell Cycle
+
+
+# install packages and load libraries
+install.packages("pacman")
+require(pacman)  
+library(pacman)  
 library(reshape2)
 library(grid)
 library(gridExtra)
 
-
-# Or, by using "pacman::p_load" you can use the p_load
-# function from pacman without actually loading pacman.
-# These are packages I load every time.
+#run pacman for downloading the necessary libraries
 pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, 
               ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, 
              stringr, tidyr, ggpubr, grid, gridExtra) 
 
-
-#tracking_id.Ad_1_FPKM.Ad_2_FPKM.P0_2_FPKM.P4_1_FPKM.P4_2_FPKM.P7_1_FPKM.P7_2_FPKM
+# read the CSV's for the different developmental phases into a dataframe
 fpkm_matrix <- read.csv("/project/bf528/project_2/data/fpkm_matrix.csv", sep = "\t")
-
-#tracking_id.class_code.nearest_ref_id.gene_id.gene_short_name.tss_id.locus.length.coverage.FPKM.FPKM_conf_lo.FPKM_conf_hi.FPKM_status
 gene_table_P0_1 <-  read.csv("/projectnb/bf528/users/van-gogh/project_2/programmer/P0_1_cufflinks/genes.fpkm_tracking", sep = "\t")
 gene_table_P0_2 <-  read.csv("/project/bf528/project_2/data/samples/P0_2/genes.fpkm_tracking",sep = "\t")
 gene_table_P4_1 <-  read.csv("/project/bf528/project_2/data/samples/P4_1/genes.fpkm_tracking", sep = "\t")
 gene_table_P4_2 <-  read.csv("/project/bf528/project_2/data/samples/P4_2/genes.fpkm_tracking", sep = "\t")
 gene_table_P7_1 <-  read.csv("/project/bf528/project_2/data/samples/P7_1/genes.fpkm_tracking", sep = "\t")
 gene_table_P7_2 <-  read.csv("/project/bf528/project_2/data/samples/P7_2/genes.fpkm_tracking", sep = "\t")
-
-
 gene_table_Ad_1 <- read.csv("/project/bf528/project_2/data/samples/Ad_1/genes.fpkm_tracking", sep = "\t")
 gene_table_Ad_2 <- read.csv("/project/bf528/project_2/data/samples/Ad_2/genes.fpkm_tracking", sep = "\t")
 
+#7.1
 
+#list of genes for each Sacromere, Mitochondria and Cell Cylce
 list_sarcomere_gene <- list("Pdlim5","Pygm","Myoz2","Des","Csrp3", "Tcap","Cryab")
-
 list_mitochondria_gene <- list("Mpc1","Prdx3","Acat1","Echs1","Slc25a11","Phyh")
-
 list_cell_cyle_gene <- list("Cdc7","E2f8","Cdk7","Cdc26","Cdc6","Cdc27", "E2f1","Bora","Cdc45","Rad51","Aurkb","Cdc23")
 
-
+#function to return the fpkm values of the list of genes for Sacromere, Mitochondria
 fpkm_values <- function(list_gene_names, gene_phase_table) {
   list_fpkm <- c()
   i <- 1
@@ -57,6 +53,7 @@ fpkm_values <- function(list_gene_names, gene_phase_table) {
   return(list_fpkm)
 }
 
+#function to return the fpkm values of the list of genes for cell cycle
 fpkm_values_cycle_cycle <- function(list_gene_names, gene_phase_table) {
   list_fpkm <- c()
   i <- 1
@@ -78,6 +75,7 @@ fpkm_values_cycle_cycle <- function(list_gene_names, gene_phase_table) {
   return(list_fpkm)
 }
 
+#function to plot the line graph 
 plot_function <- function(list_gene, df_to_return){
   
   df_to_return <- as.data.frame(t(df_to_return))
@@ -96,29 +94,28 @@ plot_function <- function(list_gene, df_to_return){
 }
 
 
-
 #sarcomere_1
 
+#get the FPKM vlaues for the sarcomere
 S_P0_1 <- fpkm_values(list_sarcomere_gene, gene_table_P0_1)
-
 S_P4_1 <- fpkm_values(list_sarcomere_gene, gene_table_P4_1)
-
 S_P7_1 <- fpkm_values(list_sarcomere_gene, gene_table_P7_1)
-
 S_Ad_1 <- fpkm_values(list_sarcomere_gene, gene_table_Ad_1)
 
+#create the dataframe of different phases
 df_sarcomere<- data.frame(S_P0_1, S_P4_1,  
                           S_P7_1,  S_Ad_1)
 
+#plot the line plot
 df_to_plot <- plot_function(list_sarcomere_gene,df_sarcomere)
-
-
 level_order <- c("S_P0_1", "S_P4_1","S_P7_1",  "S_Ad_1")
 
+#save the line plot
 pdf(file = "/projectnb/bf528/users/van-gogh/project_2/biologist/Sarcomere_Sample_1.png",  
     width = 4, # The width of the plot in inches
     height = 4)
 
+# plot the line plot
 p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value , group=Genes)) +
   geom_line(aes(color=Genes))+
   geom_point()
@@ -129,28 +126,23 @@ sarcomere_1 <- p + ggtitle("Sarcomere_Sample_1") +
 ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/Sarcomere_Sample_1.png")
 
 
-# sarcomere_1
-
 #sarcomere_2
 
+#get the FPKM values 
 S_P0_2 = fpkm_values(list_sarcomere_gene, gene_table_P0_2)
-
 S_P4_2 = fpkm_values(list_sarcomere_gene, gene_table_P4_2)
-
 S_P7_2 = fpkm_values(list_sarcomere_gene, gene_table_P7_2)
-
 S_Ad_2 = fpkm_values(list_sarcomere_gene, gene_table_Ad_2)
 
-
+#create the dataframe of different phases
 df_sarcomere<- data.frame(S_P0_2, S_P4_2,  
                           S_P7_2,  S_Ad_2)
 
 df_to_plot <- plot_function(list_sarcomere_gene,df_sarcomere)
 
-df_to_plot
-
 level_order <- c("S_P0_2", "S_P4_2","S_P7_2",  "S_Ad_2")
 
+# plot the line plot
 p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value , group=Genes)) +
   geom_line(aes(color=Genes))+
   geom_point()
@@ -160,44 +152,31 @@ sarcomere_2 <- p + ggtitle("Sarcomere_Sample_2") +
 
 ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/Sarcomere_Sample_2.png")
 
-# 
-# sarcomere_2
-# 
-# ggarrange(sarcomere_2, sarcomere_1, ncol = 2, nrow = 1, common.legend = TRUE)
-
-# grid.arrange(sarcomere_1, sarcomere_2, nrow = 1)
-
-
-
 
 #mitochondria_1
 
+#get the FPKM values 
 M_P0_1 = fpkm_values(list_mitochondria_gene, gene_table_P0_1)
-
 M_P0_1[is.na(M_P0_1)] <- 0
 
 M_P4_1 = fpkm_values(list_mitochondria_gene, gene_table_P4_1)
-
 M_P4_1[is.na(M_P4_1)] <- 0
 
 M_P7_1 = fpkm_values(list_mitochondria_gene, gene_table_P7_1)
-
 M_P7_1[is.na(M_P7_1)] <- 0
 
 M_Ad_1 = fpkm_values(list_mitochondria_gene, gene_table_Ad_1)
-
 M_Ad_1[is.na(M_Ad_1)] <- 0
 
+#create the dataframe of different phases
 df_mitochondria<- data.frame(M_P0_1, M_P4_1,  
                           M_P7_1,  M_Ad_1)
 
-df_mitochondria
 df_to_plot <- plot_function(list_mitochondria_gene,df_mitochondria)
-
-df_to_plot
 
 level_order <- c("M_P0_1", "M_P4_1","M_P7_1",  "M_Ad_1")
 
+# plot the line plot
 p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value , group=Genes)) +
   geom_line(aes(color=Genes))+
   geom_point()
@@ -205,40 +184,33 @@ p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value ,
 mict_1 <- p + ggtitle("Mitochondria_Sample_1") +
   xlab("Phases") + ylab("FPKM") + theme(plot.title = element_text(hjust = 0.5))
 
-
 ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/Mitochondria_Sample_1.png")
 
 
-# mict_1
-
 #mitochondria_2
 
+#get the FPKM values 
 M_P0_2 = fpkm_values(list_mitochondria_gene, gene_table_P0_2)
-
 M_P0_2[is.na(M_P0_2)] <- 0
 
 M_P4_2 = fpkm_values(list_mitochondria_gene, gene_table_P4_2)
-
 M_P4_2[is.na(M_P4_2)] <- 0
 
 M_P7_2 = fpkm_values(list_mitochondria_gene, gene_table_P7_2)
-
 M_P7_2[is.na(M_P7_2)] <- 0
 
 M_Ad_2 = fpkm_values(list_mitochondria_gene, gene_table_Ad_2)
-
 M_Ad_2[is.na(M_Ad_2)] <- 0
 
-
+#create the dataframe of different phases
 df_mitochondria<- data.frame(M_P0_2, M_P4_2,  
                              M_P7_2,  M_Ad_2)
 
 df_to_plot <- plot_function(list_mitochondria_gene,df_mitochondria)
 
-df_to_plot
-
 level_order <- c("M_P0_2", "M_P4_2","M_P7_2",  "M_Ad_2")
 
+# plot the line plot
 p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value , group=Genes)) +
   geom_line(aes(color=Genes))+
   geom_point()
@@ -249,32 +221,23 @@ mict_2 <- p + ggtitle("Mitochondria_Sample_2") +
 
 ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/Mitochondria_Sample_2.png")
 
-
-# mict_2
-
-# figure_mict <- ggarrange(mict_1, mict_2)
-# 
-# figure_mict
-
 #cycle_cycle_1
 
+#get the FPKM values 
 C_P0_1 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_P0_1)
-
 C_P4_1 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_P4_1)
-
 C_P7_1 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_P7_1)
-
 C_Ad_1 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_Ad_1)
 
+#create the dataframe of different phases
 df_cycle_cycle<- data.frame(C_P0_1, C_P4_1,  
                              C_P7_1,  C_Ad_1)
 
 df_to_plot <- plot_function(list_cell_cyle_gene,df_cycle_cycle)
 
-df_to_plot
-
 level_order <- c("C_P0_1", "C_P4_1","C_P7_1",  "C_Ad_1")
 
+# plot the line plot
 p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value , group=Genes)) +
   geom_line(aes(color=Genes))+
   geom_point()
@@ -282,39 +245,31 @@ p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value ,
 cell_cycle_1 <- p + ggtitle("Cell_Cycle_Sample_1") +
   xlab("Phases") + ylab("FPKM") + theme(plot.title = element_text(hjust = 0.5))
 
-cell_cycle_1
-
 ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/Cell_Cycle_Sample_1.png")
 
 #cycle_cycle_2
 
-
+#get the FPKM values 
 C_P0_2 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_P0_2)
-
 C_P4_2 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_P4_2)
-
 C_P7_2 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_P7_2)
-
 C_Ad_2 = fpkm_values_cycle_cycle(list_cell_cyle_gene, gene_table_Ad_2)
 
-
+#create the dataframe of different phases
 df_cycle_cycle<- data.frame(C_P0_2, C_P4_2,  
                             C_P7_2,  C_Ad_2)
 
 df_to_plot <- plot_function(list_cell_cyle_gene,df_cycle_cycle)
 
-df_to_plot
-
 level_order <- c("C_P0_2", "C_P4_2","C_P7_2",  "C_Ad_2")
 
+# plot the line plot
 p <- ggplot(data=df_to_plot, aes(x=factor(Phase, level = level_order), y=value , group=Genes)) +
   geom_line(aes(color=Genes))+
   geom_point()
 
 cell_cycle_2 <- p + ggtitle("Cell_Cycle_Sample_2") +
   xlab("Phases") + ylab("FPKM") + theme(plot.title = element_text(hjust = 0.5))
-
-cell_cycle_2
 
 ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/Cell_Cycle_Sample_2.png")
 
--- headmap.R
+++ headmap.R
@@ -1,83 +1,60 @@
-install.packages("pacman")
+# Monil Gandhi - Biologist
 
-# Then load the package by using either of the following:
-require(pacman)  # Gives a confirmation message.
-library(pacman)  # No message.
+#File Description - 1) Creates a heatmap of the top 150 differentially expressed genes.
+#                   2) Comparison of the DAVID functional annotation clustering data to the reference paper data
+
+# install packages and load libraries
+install.packages("pacman")
+require(pacman)  
+library(pacman)  
 library(reshape2)
 library(grid)
 library(gridExtra)
 library(magrittr)
-
 library(tibble)
 
-
-
-
-# Or, by using "pacman::p_load" you can use the p_load
-# function from pacman without actually loading pacman.
-# These are packages I load every time.
+#run pacman for downloading the necessary libraries
 pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, 
                ggvis, httr, lubridate, plotly, rio, rmarkdown, shiny, 
                stringr, tidyr, ggpubr, grid, gridExtra, pheatmap, gdata, magrittr) 
 
 
-#tracking_id.Ad_1_FPKM.Ad_2_FPKM.P0_2_FPKM.P4_1_FPKM.P4_2_FPKM.P7_1_FPKM.P7_2_FPKM
+# read the CSV's for the different developmental phases into a dataframe
 fpkm_matrix <- read.csv("/project/bf528/project_2/data/fpkm_matrix.csv", sep = "\t")
-
-#tracking_id.class_code.nearest_ref_id.gene_id.gene_short_name.tss_id.locus.length.coverage.FPKM.FPKM_conf_lo.FPKM_conf_hi.FPKM_status
 gene_table_P0_1 <-  read.csv("/projectnb/bf528/users/van-gogh/project_2/programmer/P0_1_cufflinks/genes.fpkm_tracking", sep = "\t")
 gene_table_P0_2 <-  read.csv("/project/bf528/project_2/data/samples/P0_2/genes.fpkm_tracking",sep = "\t")
 gene_table_P4_1 <-  read.csv("/project/bf528/project_2/data/samples/P4_1/genes.fpkm_tracking", sep = "\t")
 gene_table_P4_2 <-  read.csv("/project/bf528/project_2/data/samples/P4_2/genes.fpkm_tracking", sep = "\t")
 gene_table_P7_1 <-  read.csv("/project/bf528/project_2/data/samples/P7_1/genes.fpkm_tracking", sep = "\t")
 gene_table_P7_2 <-  read.csv("/project/bf528/project_2/data/samples/P7_2/genes.fpkm_tracking", sep = "\t")
-
 gene_table_Ad_1 <- read.csv("/project/bf528/project_2/data/samples/Ad_1/genes.fpkm_tracking", sep = "\t")
 gene_table_Ad_2 <- read.csv("/project/bf528/project_2/data/samples/Ad_2/genes.fpkm_tracking", sep = "\t")
-
 diff_exp <- read.csv("/projectnb/bf528/users/van-gogh/project_2/programmer/cuffdiff_out/gene_exp.diff", sep = "\t", stringsAsFactors = F)
 
 
+# 7.3
+
+#concatenate the different phases and their FPKM values
 combined_df <- merge(fpkm_matrix, gene_table_P0_1, by="tracking_id")
-
 names(combined_df)[names(combined_df) == 'FPKM'] <- 'P0_1_FPKM'
-
 ordered_df_q_value <- diff_exp[order(diff_exp$q_value),]
-
 diff_exp_1k <- ordered_df_q_value[1:1000,]
-
 diff_exp_significant <- diff_exp_1k[diff_exp_1k$significant == "yes", ]
-
-
 df_final_ordered_diff_exp <- diff_exp_significant[1:200,]
-
 fpkm_value_diff_exp = combined_df[combined_df$gene_short_name %in% df_final_ordered_diff_exp$gene,]
-
-
 fpkm_diff_exp_final = fpkm_value_diff_exp[complete.cases(fpkm_value_diff_exp), ]
-
 fpkm_diff_exp_final_nd = fpkm_diff_exp_final[!duplicated(fpkm_diff_exp_final$gene_short_name),]
 
-
+#create the final dataframe 
 fpkm_diff_exp_final_nd <- fpkm_diff_exp_final_nd[,c("Ad_1_FPKM","Ad_2_FPKM","P0_1_FPKM", "P0_2_FPKM", "P4_1_FPKM", "P4_2_FPKM", "P7_1_FPKM", "P7_2_FPKM", "gene_short_name")]
-
 rownames(fpkm_diff_exp_final_nd)<-fpkm_diff_exp_final_nd$gene_short_name
-
 fpkm_diff_exp_final_nd <- fpkm_diff_exp_final_nd[,c("Ad_1_FPKM","Ad_2_FPKM","P0_1_FPKM", "P0_2_FPKM", "P4_1_FPKM", "P4_2_FPKM", "P7_1_FPKM", "P7_2_FPKM")]
-
-fpkm_diff_exp_final_nd
-
 fpkm_diff_exp_final_nd <- fpkm_diff_exp_final_nd[!(apply(fpkm_diff_exp_final_nd, 1, function(y) any(y == 0))),]
 
+# create the matrix out of the dataframe to provide it as an input to the heatmap
 matrix_df <- as.matrix(fpkm_diff_exp_final_nd)
 
-matrix_df
-
-set_rownames(matrix_df("Genes"))
-set_colnames(matrix_df("Samples"))
-
-
-
+#plot the heatmap
 pheatmap(matrix_df, scale = "row", fontsize_row = 4, color = colorRampPalette(c("navy", "white", "firebrick3"))(50), 
          cluster_row = T,
          cluster_col = T,
@@ -87,53 +64,45 @@ pheatmap(matrix_df, scale = "row", fontsize_row = 4, color = colorRampPalette(c(
          legend = T,
          fontsize = 10)
 
-grid.text("Genes", y=-0.07, gp=gpar(fontsize=16))
-grid.text("Samples", x=-0.07, rot=90, gp=gpar(fontsize=16))
-
-# ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/heatMap.png")
+#save the heatmap
+ggsave("/projectnb/bf528/users/van-gogh/project_2/biologist/heatMap.png")
 
 
-# dev.off()
+#7.2
 
+#upregulated 
 
-
-
-
-
+#read the up regulated csv
 up_regulated <- read.csv("/projectnb/bf528/users/van-gogh/project_2/biologist/up_reg_gene_output.csv", header = F)
 
+#read the reference paper csv
 up_regulated_reference <- read.csv("/projectnb/bf528/users/van-gogh/project_2/biologist/up_reference_research_paper.csv", header = F)
 
+#create a column for overlapping row called similar_row_values after comparing the two csv's
 similar_row_values <- match(up_regulated[,2], up_regulated_reference[,2])
-
 similar_row_values <- ifelse(is.na(similar_row_values), "No", "Yes")
-
 go_terms_up_regulated <- cbind(up_regulated, similar_row_values)
+table(go_terms_up_regulated$similar_row_values)
 
-# go_terms_up_regulated_clusters <- table_upgo[1:90,]
+#write the csv for the upregulated genes
+write.csv(go_terms_up_regulated,"up_regulated_GO_clusters.csv")
 
-# write.csv(go_terms_up_regulated,"up_regulated_GO_clusters.csv")
+#downregulated
 
-
-
-
+#read the up downregulated csv
 down_regulated <- read.csv("/projectnb/bf528/users/van-gogh/project_2/biologist/down_reg_gene_output.csv", header = F)
 
+#read the reference paper csv
 down_regulated_reference <- read.csv("/projectnb/bf528/users/van-gogh/project_2/biologist/down_reference_research_paper.csv", header = F)
 
+#create a column for overlapping row called similar_row_values after comparing the two csv's
 similar_row_values <- match(down_regulated[,2], down_regulated_reference[,2])
-
 similar_row_values <- ifelse(is.na(similar_row_values), "No", "Yes")
-
 go_terms_down_regulated <- cbind(down_regulated, similar_row_values)
+table(go_terms_down_regulated$similar_row_values)
 
-# go_terms_up_regulated_clusters <- table_upgo[1:90,]
-
-# write.csv(go_terms_down_regulated,"down_regulated_GO_clusters.csv")
-
-
-
-
+#write the csv for the downregulated genes
+write.csv(go_terms_down_regulated,"down_regulated_GO_clusters.csv")
 
 
 
